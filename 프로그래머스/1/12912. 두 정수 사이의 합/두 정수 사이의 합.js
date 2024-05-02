eb354fa9f892cd72de2329de0a0203fbc6b9aefb
KangSanLee24 <166798556+KangSanLee24@users.noreply.github.com>
2024-05-02 09:24:00 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,12 @@
+function solution(a, b) {
+    var answer = 0;
+    
+    let min = Math.min(a, b);
+    let max = Math.max(a, b);
+    
+    for(i=min;i<=max;i++) {
+        answer +=i;
+    }
+    
+    return answer;
+}
