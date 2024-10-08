f4d7de5e2988d336807645073741fe725b132cdb
KangSanLee24 <166798556+KangSanLee24@users.noreply.github.com>
2024-10-08 18:00:37 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 11.74 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
@@ -2,7 +2,7 @@ def solution(arr, queries):
     
     for s, e, k in queries:
         for i in range(s, e + 1 ):
-            if i % k == 0:
+            if i == 0 or i % k == 0:
                 arr[i] += 1
     
     return arr
